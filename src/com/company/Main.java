161a91d9dd5fa6ba272d6ea9daa9c677783e66b1
Kirill Tapkov <izhjup@mail.ru>
2021-12-13 00:43:04 +0400

    Add *.xml and *.html to gitignore

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("1.");
+        System.out.println("2..");
+        System.out.println("3...");
+	    System.out.println("Hello World!");
+    }
+}
