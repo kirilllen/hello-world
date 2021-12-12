3145cf45677147b6a0eb6d84480ba8de5c94f0dc
Kirill Tapkov <izhjup@mail.ru>
2021-12-13 00:51:53 +0400

    Program with i+1 function

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("1.");
-        System.out.println("2..");
-        System.out.println("3...");
-	    System.out.println("Hello World!");
+        int i=1;
+        System.out.println(i);
+        i=i+1;
+        System.out.println(i);
+        i=i+1;
+        System.out.println(i);
     }
 }
